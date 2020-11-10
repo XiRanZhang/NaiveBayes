c79a103c5c3f51f934ae2c5301724df19d0801b2
XiRanZhang <zhangxiranluna@gmail.com>
2020-11-10 20:13:53 +0800

    add Evaluation,complete local program.

--- src/main/java/com/zhang/hadoop/Evaluation.java
+++ src/main/java/com/zhang/hadoop/Evaluation.java
@@ -9,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import java.util.*;
+
 public class Evaluation {
 
 
@@ -26,12 +28,69 @@ public class Evaluation {
             inputStream=fileSystem.open(resultOfClassficationPath);
             bfreader = new BufferedReader(new InputStreamReader(inputStream));
             String line=null;
+            //用于计算每一类的TP
+            //real class name;a[0]:the real number of the docs in the class
+            //a[1]: the prediction number of the docs in the class
+            //a[2]:the prediction and real class docs in the class
+            HashMap<String,Integer[]> classifierMap=new HashMap<>();
+
             //read <classname,docNum> into hash map classPriorProbability
             while (( line = bfreader.readLine()) != null) {
                 String[] tokens = line.split("\t");
                 String realClass=tokens[0].split("\\+")[0];
-                String classifier =tokens[1];
+                String classifierClass =tokens[1];
+
+                //calculate precision here.
+                if (realClass.equals(classifierClass)){
+                    if (classifierMap.containsKey(realClass)){
+                        Integer[] vals=classifierMap.get(realClass);
+                        for (int i=0;i<vals.length;i++) vals[i] +=1;
+                        classifierMap.put(realClass,vals);
+                    }else {
+                        Integer[] vals={1,1,1};
+                        classifierMap.put(realClass,vals);
+                    }
+                }else{
+                    if (classifierMap.containsKey(realClass)){
+                        Integer[] vals=classifierMap.get(realClass);
+                        vals[0] +=1;
+                        classifierMap.put(realClass,vals);
+                    }else {
+                        Integer[] vals={1,0,0};
+                        classifierMap.put(realClass,vals);
+                    }
+                    if (classifierMap.containsKey(classifierClass)){
+                        Integer[] vals=classifierMap.get(classifierClass);
+                        vals[1] +=1;
+                        classifierMap.put(classifierClass,vals);
+                    }else {
+                        Integer[] vals={0,1,0};
+                        classifierMap.put(classifierClass,vals);
+                    }
+                }
+
             }
+            for (Integer val:classifierMap.get("AUSTR")){
+                System.out.println(val);
+            }
+            for (Integer val:classifierMap.get("CANA")){
+                System.out.println(val);
+            }
+            //use Macro-Average here
+            double precision=0.0,recall=0.0;
+            for (Map.Entry<String, Integer[]> entry : classifierMap.entrySet()) {
+                Integer[] vals = entry.getValue();
+                double tempPreci = (vals[2] + 0.0) / vals[1];
+                precision+=tempPreci;
+                double tempRecall = (vals[2] + 0.0) / vals[0];
+                recall+=tempRecall;
+                System.out.println(entry.getKey()+"\t"+"precision: "+tempPreci+"\t"+"recall: "+tempRecall);
+            }
+            recall =recall/classifierMap.size();
+            precision =precision/classifierMap.size();
+            System.out.println("precision: "+precision+"\t"+"recall: "+recall);
+            double f1=2*(precision*recall)/(precision+recall);
+            System.out.println("Macro-Avarage here: "+f1);
         }catch (IOException e){
             e.printStackTrace();
         }finally {
--- src/main/java/com/zhang/hadoop/Main.java
+++ src/main/java/com/zhang/hadoop/Main.java
@@ -7,34 +7,35 @@ public class Main {
     public static void main(String[] args) throws Exception{
 
         Configuration conf = new Configuration();
-//        //converter small file in training data set in sequence file
-//        conf.set("INPUTPATH",Utils.BASE_TRAINDATA_PATH);
-//        conf.set("OUTPUTPATH",Utils.SEQUENCE_TRAIN_DATA);
+        //converter small file in training data set in sequence file
+        conf.set("INPUTPATH",Utils.BASE_TRAINDATA_INPUTPATH);
+        conf.set("OUTPUTPATH",Utils.SEQUENCE_TRAIN_DATA);
 
         //converter small files in test data in sequence file
-//        conf.set("INPUTPATH",Utils.BASE_TESTDATA_INPUTPATH);
-//        conf.set("OUTPUTPATH",Utils.SEQUENCE_TEST_DATA);
-//        SmallFilesToSequenceFileConverter sequenceFileConverter = new SmallFilesToSequenceFileConverter();
-//        ToolRunner.run(conf, sequenceFileConverter, args);
+        conf.set("INPUTPATH",Utils.BASE_TESTDATA_INPUTPATH);
+        conf.set("OUTPUTPATH",Utils.SEQUENCE_TEST_DATA);
+        SmallFilesToSequenceFileConverter sequenceFileConverter = new SmallFilesToSequenceFileConverter();
+        ToolRunner.run(conf, sequenceFileConverter, args);
 
-//        //calculate the number of files in each class
-//        conf.set("INPUTPATH",Utils.SEQUENCE_TRAIN_DATA);
-//        conf.set("OUTPUTPATH",Utils.DOCs_NUM_OF_EACH_CLASS_OUTPUT_PATH);
-//        DocsNumOfEachClass docsNumOfEachClass =new DocsNumOfEachClass();
-//        ToolRunner.run(conf,docsNumOfEachClass,args);
-//
-//        //calculate the number of each term in each class
-//        conf.set("INPUTPATH",Utils.SEQUENCE_TRAIN_DATA);
-//        conf.set("OUTPUTPATH",Utils.TERMs_NUM_OF_EACH_CLASS_OUTPUT_PATH);
-//        TermsNumOfEachClass termsNumOfEachClass=new TermsNumOfEachClass();
-//        ToolRunner.run(conf,termsNumOfEachClass,args);
+        //calculate the number of files in each class
+        conf.set("INPUTPATH",Utils.SEQUENCE_TRAIN_DATA);
+        conf.set("OUTPUTPATH",Utils.DOCs_NUM_OF_EACH_CLASS_OUTPUT_PATH);
+        DocsNumOfEachClass docsNumOfEachClass =new DocsNumOfEachClass();
+        ToolRunner.run(conf,docsNumOfEachClass,args);
+
+        //calculate the number of each term in each class
+        conf.set("INPUTPATH",Utils.SEQUENCE_TRAIN_DATA);
+        conf.set("OUTPUTPATH",Utils.TERMs_NUM_OF_EACH_CLASS_OUTPUT_PATH);
+        TermsNumOfEachClass termsNumOfEachClass=new TermsNumOfEachClass();
+        ToolRunner.run(conf,termsNumOfEachClass,args);
 
         //calculate bayes function
         conf.set("INPUTPATH",Utils.SEQUENCE_TEST_DATA);
         conf.set("OUTPUTPATH",Utils.RESULT_OF_CLASSFICATION);
         NaiveBayes naiveBayes =new NaiveBayes();
         ToolRunner.run(conf,naiveBayes,args);
-    }
 
+        Evaluation evaluation=new Evaluation();
+    }
 
 }
